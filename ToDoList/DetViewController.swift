ece31bbd139a950b5efcf2273625ba5f29a8d8df
Christian Guma <gumac>
2019-06-14 17:50:25 +0100

    final1

---
+++ ToDoList/DetViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetViewController.swift
+//  ToDoList
+//
+//  Created by Christian Guma on 6/14/19.
+//  Copyright © 2019 Christian Guma. All rights reserved.
+//
+
+import UIKit
+
+class DetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    
+    @IBOutlet weak var toDoField: UITextField!
+    
+
+}
--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -9,12 +9,102 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tablView: UITableView!
+    @IBOutlet weak var editButton: UIBarButtonItem!
+    @IBOutlet weak var addButton: UIBarButtonItem!
+    
+    var defaultData = UserDefaults.standard
+    var toDoArray = [String]()
+    var toDoNotesArray = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tablView.delegate = self
+        tablView.dataSource = self
+        
+        toDoArray = defaultData.stringArray(forKey: "toDoArray") ?? [String]()
+        toDoNotesArray = defaultData.stringArray(forKey: "toDoNotesArray") ?? [String]()
     }
+    
+    func saveDefaultData() {
+        defaultData.set(toDoArray, forKey: "toDoArray")
+        defaultData.set(toDoNotesArray, forKey: "toDoNotesArray")
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "EditItem" {
+            let destination = segue.destination as! ViewController
+            let index = tablView.indexPathForSelectedRow!.row
+            destination.toDoNotesArray = [toDoArray[index]]
+            destination.toDoNotesArray = [toDoNotesArray[index]]
+        } else {
+            if let sPath = tablView.indexPathForSelectedRow {
+                tablView.deselectRow(at: sPath, animated: false)
+            }
+        }
+    }
+    
+    @IBAction func unwindFromDefViewController(segue: UIStoryboardSegue) {
+        let sourceViewController = segue.source as! ViewController
+        if let indexPath = tablView.indexPathForSelectedRow {
+            toDoArray[indexPath.row] = sourceViewController.toDoNotesArray
+            toDoNotesArray[indexPath.row] = ViewController
+            tablView.reloadRows(at: [indexPath], with: .automatic)
+        } else {
+            let newIndexPath = IndexPath(row: toDoArray.count, section: 0)
+            toDoArray.append(sourceViewController.toDoItem!)
+            toDoNotesArray.append(sourceViewController.toDoNoteItem!)
+            tablView.insertRows(at: [newIndexPath], with: .automatic)
+        }
+        saveDefaultData()
+    }
+    
+    @IBAction func editBarButtonPressed(_ sender: UIBarButtonItem) {
+        if tablView.isEditing {
+            tablView.setEditing(false, animated: true)
+            editButton.title = "Edit"
+            addButton.isEnabled = true
+        } else {
+            tablView.setEditing(true, animated: true)
+            editButton.title = "Done"
+            addButton.isEnabled = false
+        }
+    }
+    
+}
 
-
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = toDoArray[indexPath.row]
+        cell.detailTextLabel?.text = toDoNotesArray[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            toDoArray.remove(at: indexPath.row)
+            toDoNotesArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            saveDefaultData()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let itemToMove = toDoArray[sourceIndexPath.row]
+        let noteToMove = toDoNotesArray[sourceIndexPath.row]
+        toDoArray.remove(at:sourceIndexPath.row)
+        toDoNotesArray.remove(at: sourceIndexPath.row)
+        toDoArray.insert(itemToMove, at: destinationIndexPath.row)
+        toDoNotesArray.insert(noteToMove, at: destinationIndexPath.row)
+        saveDefaultData()
+    }
+    
+    
 }
 
